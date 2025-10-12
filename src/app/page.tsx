40514a9117cd3927e0fb1f4789d79175732d0532
Nischal <nyyscal541556@gmail.com>
2025-10-12 14:26:45 +0545

    Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import CTA from "@/components/landing/CTA";
+import Footer from "@/components/landing/Footer";
 import Header from "@/components/landing/Header";
 import Hero from "@/components/landing/Hero";
 import HowItWorks from "@/components/landing/HowItWorks";
 import PricingSection from "@/components/landing/PricingSection";
 import WhatToAsk from "@/components/landing/WhatToAsk";
-import { Footer } from "react-day-picker";
 
 export default function Home() {
   return (
